3601fe6a5432fea49bf36368dde033fbb3f096ec
hector vanegas <h.vanegas@utp.edu.co>
2024-10-08 18:05:42 -0500

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryFormComponent } from './components/molecules/category-form/category-form.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/dashboard/home/home.component';
+import { CategoriesComponent } from './components/pages/dashboard/categories/categories.component';
+import { CategoryFormComponent } from './components/molecules/category-form/category-form.component';  // Asegúrate de importar tu CategoryFormComponent
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'category-form', component: CategoryFormComponent }  
+    {
+        path: 'login',
+        title: 'Emazon',
+        component: LoginComponent,
+    },
+    {
+        path: 'dashboard',
+        title: 'Emazon',
+        component: CategoryFormComponent,  
+        children: [
+            {
+                path: '',
+                component: HomeComponent,
+            },
+            {
+                path: 'categories',
+                title: 'Categories',
+                component: CategoriesComponent,
+            },
+        ],
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
